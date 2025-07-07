366747a27f5968252ca81fad443a602a37c87a56
SergioComeron <sergiocomeron@icloud.com>
2025-07-07 11:09:46 +0200

    Initial Commit

---
+++ TimestampConverter/TimestampConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimestampConverterApp.swift
+//  TimestampConverter
+//
+//  Created by Sergio Comerón on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct TimestampConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
